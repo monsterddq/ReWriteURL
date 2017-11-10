38c6827e250c4fbf1784bfcebc751c81e0368376
monterddq <thaidq@d3plus.com>
2017-11-10 10:15:29 +0700

    Add project files.

---
+++ ReWriteURL.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReWriteURL.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
